a53996612343e30a889848d08b5e3cd3a60a5a81
shm_usuf <shmusuf@gmail.com>
2023-01-24 21:30:45 +0600

    qeestion one added

--- question-one.js
+++ question-one.js
@@ -6,8 +6,6 @@
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - (i + 1); j++) {
-      console.log("to see", arr, arr[j], arr[j + 1]);
-
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
